db9d3c38924a2935fee247b32f4ffa05524e865f
bbernays <ben@bernays.me>
2022-01-04 08:32:47 -0500

    Update spaces.go

--- resources/spaces.go
+++ resources/spaces.go
@@ -2,7 +2,6 @@ package resources
 
 import (
 	"context"
-	"log"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
@@ -149,14 +148,19 @@ func Spaces() *schema.Table {
 
 func fetchSpaces(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- interface{}) error {
 	svc := meta.(*client.Client)
+	log := meta.Logger()
+
 	buckets, err := svc.S3.ListBuckets(ctx, &s3.ListBucketsInput{}, func(options *s3.Options) {
 		options.Region = svc.SpacesRegion
 	})
 	if err != nil {
-		log.Println(err)
+		log.Error("Failed to List Buckets", "Error", err)
+		if !svc.CredentialStatus.Spaces {
+			log.Warn("Spaces credentials not set")
+		}
 		return err
 	}
-	log.Println(buckets.Buckets)
+
 	wb := make([]*WrappedBucket, len(buckets.Buckets))
 	for i, b := range buckets.Buckets {
 		wb[i] = &WrappedBucket{
