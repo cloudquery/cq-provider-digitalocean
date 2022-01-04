4ae9136f30251e0793d3691d7e3fc54bad68baf8
bbernays <ben@bernays.me>
2022-01-04 12:16:21 -0500

    Update variables.tf

--- resources/integration_tests/infra/variables.tf
+++ resources/integration_tests/infra/variables.tf
@@ -3,15 +3,11 @@ variable "test_suffix" {
   default = ""
 }
 
-variable "test_prefix" {
-  type = string
-  default = ""
-}
 
 
 resource "random_id" "test_id" {
   keepers = {
-    test_ids = "${var.test_prefix}${var.test_suffix}"
+    test_ids = "${var.test_suffix}"
   }
 
   byte_length = 8
